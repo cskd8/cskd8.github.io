294900d2fc8c012fbdb31fdc129871758b27035c
hirocy <mh4869203@gmail.com>
2023-02-13 07:32:57 +0000

    :bug: fix SECCON discription

--- src/pages/Achievements.vue
+++ src/pages/Achievements.vue
@@ -11,7 +11,7 @@
             <li>ICTSC2021 夏の陣 - 8位</li>
             <li><a href="https://isucon.net/archives/56021237.html">ISUCON11</a> - 予選学生3位、全体35位 本選出場</li>
             <li><a href="https://icttoracon.net/archives/8962">ICTSC2021 冬の陣</a> - 3位</li>
-            <li><a href="https://www.seccon.jp/2022/seccon_ctf/result_of_seccon_ctf_2022_quals.html">SECCON CTF 2022</a> - 予選国内6位、全体31位 本選出場</li>
+            <li><a href="https://www.seccon.jp/2022/seccon_ctf/result_of_seccon_ctf_2022_quals.html">SECCON CTF 2022</a> - 予選国内6位、全体31位 国内決勝出場</li>
           </ul>
           <br>
           <br>
